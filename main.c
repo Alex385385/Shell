90245812b579ef9abd0496ca40fdc3fe805a8416
Alejandro Millan <alejandro_millan@ymail.com>
2020-10-13 13:33:05 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <dirent.h>
+
+//cd - Change directory
+//Use chdir()
+void changeDirectory() {
+
+}
+
+//pwd - Present Working directory
+//Use getcwd()
+void printCurrentDirectory() {
+
+}
+
+//mkdir - Make a directory (Alerts if already exists)
+//Use mkdir()
+void makeDirectory() {
+
+}
+
+//rmdir - Remove the directory (Alerts if no such file or directory)
+//Use rmdir()
+void removeDirectory() {
+
+}
+
+//ls - List contents of pwd
+//Look at dirent.h library
+void lsCommand() {
+
+}
+
+//cp - Copy contents from one file to another
+//Use fgetc() and fputc()
+void copyFile() {
+
+}
+
+//
+void runExecutable() {
+
+}
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
