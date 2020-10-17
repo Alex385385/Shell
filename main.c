14bda5e950f59c1645dfcb3e4d5dc385099dbe3a
QualityBeef <khoivu617@gmail.com>
2020-10-17 16:40:47 -0700

    changes

--- main.c
+++ main.c
@@ -2,19 +2,42 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <string.h>
+#include <gtk-3.0/gtk/gtk.h> 
+#include <gtk-3.0/gtk/gtkx.h> 
+#include <signal.h>
+#include <math.h>
+#include <ctype.h>
+#include <sys/types.h>
 
-enum commands{cd, pwd, mkdir, rmdir, ls, cp};
+GtkWidget *window;
+GtkWidget *textfield;
+GtkBuilder *builder;
 
 //cd - Change directory
 //Use chdir()
-void changeDirectory() {
+void changeDirectory(char* dir) {
 
+    char s[100];
+    char *ptr;
+
+    chdir(dir);
+
+    // display new current dir
+    ptr = getcwd(s, 100);
+
+    free(s);
+    free(ptr);
 }
 
 //pwd - Present Working directory
-//Use getcwd()
-void printCurrentDirectory(char[] s) {
-    printf("/n" + getcwd(s,100) + "\n");
+// Use getcwd()
+void printCurrentDirectory() {
+
+    char s[100];
+    printf("%s\n", getcwd(s, 100));
+
+    free(s);
 }
 
 //mkdir - Make a directory (Alerts if already exists)
@@ -69,26 +92,26 @@ void exitCommand() {
 }
 
 void readCommand(char* cmd, char* args) {
-    if (sizeof(cmd) == NULL) {
-        //call method to restart newline
-    }
+//     if (sizeof(cmd) == NULL) {
+//         //call method to restart newline
+//     }
      
-    switch (cmd) {
-    case "cd":
-        changeDirectory(args);
-    case "pwd":
-        printCurrentDirectory();
-    case "mkdir":
-        makeDirectory(args);
-    case "rmdir":
-        removeDirectory(args);
-    case "ls":
-        lsCommand();
-    case "cp":
-        copyFile(args);
-    default:
-        break;
-    } 
+//     switch (cmd) {
+//     case "cd":
+//         changeDirectory(args);
+//     case "pwd":
+//         printCurrentDirectory();
+//     case "mkdir":
+//         makeDirectory(args);
+//     case "rmdir":
+//         removeDirectory(args);
+//     case "ls":
+//         lsCommand();
+//     case "cp":
+//         copyFile(args);
+//     default:
+//         break;
+//     } 
 }
 
 //
@@ -96,7 +119,31 @@ void runExecutable() {
 
 }
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+//char *argv[] is an array of char pointers
+
+int main(int argc, char *argv[] ) {
+
+    gtk_init(&argc, &argv);
+
+//----------------------------------------------------------------
+// establish contact with xml code used to adjust widget settings
+//----------------------------------------------------------------
+
+    //any gtk method will return an address of the data structure.
+    //Thus, to reference the data structure and not the address 
+    //of the data structure, pointers are used to point to the
+    //actual data structure
+
+    builder = gtk_builder_new_from_file("terminal.glade");
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "terminal_window"));
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_builder_connect_signals(builder, NULL);
+
+    textfield = GTK_WIDGET(gtk_builder_get_object(builder, "terminal_textfield"));
+
+    
+
+    gtk_widget_show(window);
+    gtk_main();
+    return 0; 
 }
